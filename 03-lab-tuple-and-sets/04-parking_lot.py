cb7fe2204ba6ff3f3c23d9d1d85a41372aa55c23
Sammy Mishinev <sam.mishinev@gmail.com>
2024-01-18 09:55:37 +0200

    Add parking lot

---
+++ 03-lab-tuple-and-sets/04-parking_lot.py
@@ -0,0 +1,19 @@
+n = int(input())
+cars_in_park = set()
+
+for _ in range(n):
+    command = input().split(", ")
+
+    direction = command[0]
+    number = command[1]
+
+    if direction == "IN":
+        cars_in_park.add(number)
+    elif direction == "OUT":
+        cars_in_park.remove(number)
+
+if len(cars_in_park) <= 0:
+    print("Parking Lot is empty")
+else:   
+    for car in cars_in_park:
+        print(car)
