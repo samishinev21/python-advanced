91845a6c4d72668719765c909cf8f0260ac37a39
Sammy Mishinev <sam.mishinev@gmail.com>
2024-01-21 19:18:29 +0200

    Add periodic table

---
+++ 04-excercise-tuple-and-sets/03-periodic-table.py
@@ -0,0 +1,11 @@
+n = int(input())
+unique_elements = set()
+
+for _ in range(n):
+    elements = input().split(" ")
+
+    for element in elements:
+        unique_elements.add(element)
+
+for element in unique_elements:
+    print(element)
