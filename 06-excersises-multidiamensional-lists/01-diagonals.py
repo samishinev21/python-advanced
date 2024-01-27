88575fe2863b8c116ed97fd0d08433623b1b2f37
Sammy Mishinev <sam.mishinev@gmail.com>
2024-01-27 11:46:14 +0200

    Add diagonals

---
+++ 06-excersises-multidiamensional-lists/01-diagonals.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+primary = []
+secondary = []
+matrix = []
+
+for _ in range(n):
+    row = list(map(int, input().split(", ")))
+    matrix.append(row)
+
+for i in range(n):
+    for j in range(n):
+        if i == j:
+            primary.append(matrix[i][j])
+        
+        if j == n - 1 - i:
+            secondary.append(matrix[i][j])
+
+print(f"Primary diagonal: {', '.join(primary)}. Sum: {sum(primary)}")
+print(f"Secondary diagonal: {', '.join(secondary)}. Sum: {sum(secondary)}")
